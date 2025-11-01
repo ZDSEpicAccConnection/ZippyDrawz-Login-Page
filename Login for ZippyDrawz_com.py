c43d8a97fd83e26527a07a21df429f25324fa45e
ZDSEpicAccConnection <zippydrawzstudioz@gmail.com>
2025-11-01 03:45:32 +0000

    Update Login for ZippyDrawz_com.py

--- Login for ZippyDrawz_com.py
+++ Login for ZippyDrawz_com.py
@@ -13,6 +13,32 @@ menu = input("Sign up or login")
 
 # sign up page
 if menu == "Sign up":
+   username = input("Please enter a username!")
+  password = input("please enter a password!")
+  file = open("user_data.txt", "a")
+  file.write(username + ":" + password + "\n")
+  file.close()
+  print("Signup successful!")
+
+elif menu == "login":
+  usernamelogin = input("Please enter your username")
+  passwordlogin = input("Please enter your password")
+
+  # Check if the passwords file exists
+  if os.path.exists("user_data.txt"):
+    file = open("user_data.txt", "r")
+    for line in file:
+      # Split the line into username and password
+      parts = line.strip().split(":")
+      if len(parts) == 2:
+        stored_username, stored_password = parts
+        if usernamelogin == stored_username and passwordlogin == stored_password:
+          print("Welcome to the system")
+          file.close()
+          break  # Exit the loop after successful login
+    else:
+      print("incorrect username or password")
+      file.close()
   else:
     print("Error: Password file not found. Please sign up first.")
 
@@ -34,3 +60,4 @@ if usernamelogin == ownerusername and passwordlogin == ownerpassword:
   else:
     print("Incorrect username or password")
     print("Error: Password file not found. Please sign up first.")
+
