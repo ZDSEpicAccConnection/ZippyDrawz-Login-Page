95073a2851ad9a4760147ad2bb5f2ead770e4e9e
ZDSEpicAccConnection <zippydrawzstudioz@gmail.com>
2025-11-01 03:38:37 +0000

    Update Login for ZippyDrawz_com.py

--- Login for ZippyDrawz_com.py
+++ Login for ZippyDrawz_com.py
@@ -1,10 +1,12 @@
 import os
 
-ownerusername = "Root"
-ownerpassword = "Github"
+print("Welcome to ZippyDrawzstudioz.art")
+print("Would you Like to Login or signup")
 
+ownerusername = "github"
+ownerpassword = "root"
 # switchbord
-menu = input("Sign up or login Or ownerlogin")
+menu = input("Sign up or login")
 
 # sign up page
 if menu == "Sign up":
@@ -37,12 +39,13 @@ elif menu == "login":
   else:
     print("Error: Password file not found. Please sign up first.")
 
-# owner login
-if menu == "ownerlogin":
-  print("please enter your login for the owner system")
-  usernameownerlog = input("Please enter your username")
-  passwordownerlog = input("please enter your password")
-  if usernameownerlog == ownerusername and passwordownerlog == ownerpassword:
-    print("Welcome to the Root system")
+#Owner Login
+
+if usernamelogin=ownerusername and passwordlogin=ownerpassword:
+          print("Welcome Back Zippy")
+          file.close()
+    else:
+      print("incorrect username or password")
+      file.close()
   else:
-    print("Incorrect username or password")
+    print("Error: Password file not found. Please sign up first.")
